045345105a81e41b7b095f6428a365b241b272d7
Nabila <nabila.nawshin50@gmail.com>
2025-08-02 05:05:11 +0200

    update coffee

--- index.js
+++ index.js
@@ -1,10 +1,89 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+ 
+
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_Pass)
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster.fhxucjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`;
+ 
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeesCollection = client.db('coffeeDB').collection('coffees')
+
+    // get coffee data from the server
+
+    app.get('/coffees',async(req, res)=>{
+      // const cursor = coffeesCollection.find();
+      // const result = await cursor.toArray();
+      // same 2 ta link k nicher ekta line e lekha hoise
+
+      const result = await coffeesCollection.find().toArray();
+      res.send(result);
+    });
+
+    // for find a  specific data
+
+    app.get('/coffees/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.findOne(query);
+      res.send(result);
+    })
+
+
+
+    app.post('/coffees', async(req, res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeesCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    // Delete data from the server
+
+    app.delete('/coffees/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('Coffee server is getting hotter')
